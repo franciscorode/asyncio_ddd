f5ce6358a81e372773fbf85d5bd98e2aa3dc583b
franciscorode <fran_rode@hotmail.com>
2022-10-30 10:33:38 +0100

    ♻️ refactor: improve domain event

--- asyncio_ddd/shared/domain/buses/event/domain_event.py
+++ asyncio_ddd/shared/domain/buses/event/domain_event.py
@@ -1,3 +1,4 @@
+import json
 import re
 from datetime import datetime
 from typing import Any
@@ -5,18 +6,48 @@ from uuid import UUID, uuid4
 
 from pydantic import BaseModel, Field
 
+TIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+
 
 class DomainEvent(BaseModel):
     name: str
-    id: UUID = Field(default_factory=uuid4)
-    occurred_on: datetime = Field(default_factory=datetime.utcnow)
+    event_id: UUID = Field(default_factory=uuid4)
     version: int = 1
-    attributes: dict[str, Any] = {}
+    occurred_on: datetime = Field(default_factory=datetime.utcnow)
+    attributes: dict[str, Any]
     meta: dict[str, Any] = {}
 
-    def __init__(self, **data: Any) -> None:
-        data["name"] = get_event_name(self.__class__.__name__)
-        super().__init__(**data)
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(
+            name=get_event_name(self.__class__.__name__), attributes=kwargs, **kwargs
+        )
+
+    def _get_serialized_attributes(self) -> dict[str, Any]:
+        attributes = {}
+        for key, attribute in self.attributes.items():
+            serialized_value = attribute
+            if isinstance(attribute, UUID):
+                serialized_value = str(attribute)
+            if isinstance(attribute, datetime):
+                serialized_value = attribute.strftime(TIME_FORMAT)
+            attributes[key] = serialized_value
+        return attributes
+
+    def dict(self, **_: Any) -> dict[str, Any]:
+        data = {
+            "data": {
+                "id": str(self.event_id),
+                "type": self.name,
+                "version": self.version,
+                "occurred_on": self.occurred_on.strftime(TIME_FORMAT),
+                "attributes": self._get_serialized_attributes(),
+                "meta": self.meta,
+            }
+        }
+        return data
+
+    def json(self, **_: Any) -> str:
+        return json.dumps(self.dict())
 
 
 def get_event_name(name: str) -> str:
--- asyncio_ddd/user/create/domain/events.py
+++ asyncio_ddd/user/create/domain/events.py
@@ -1,11 +1,7 @@
-from datetime import datetime
 from uuid import UUID
-
-from pydantic import Field
 
 from asyncio_ddd.shared.domain.buses.event.domain_event import DomainEvent
 
 
 class UserCreated(DomainEvent):
     user_id: UUID
-    created_at: datetime = Field(default_factory=datetime.utcnow)
