383dde5a97a9d4370e29ee50ae53842449985b48
Francisco Rodeño Sánchez <fran_rode@hotmail.com>
2024-01-10 19:30:08 +0100

    ✨ feat: retrieve user (#13)

--- asyncio_ddd/shared/domain/repositories/user_repository.py
+++ asyncio_ddd/shared/domain/repositories/user_repository.py
@@ -1,9 +1,15 @@
 from abc import ABC, abstractmethod
 
+from pydantic import UUID4
+
 from asyncio_ddd.shared.domain.entities.user import User
 
 
 class UserRepository(ABC):
     @abstractmethod
     async def save(self, user: User) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def retrieve(self, user_id: UUID4) -> User:
         raise NotImplementedError()
--- asyncio_ddd/shared/infrastructure/persistence/repositories/fake_user_repository.py
+++ asyncio_ddd/shared/infrastructure/persistence/repositories/fake_user_repository.py
@@ -1,3 +1,5 @@
+from pydantic import UUID4
+
 from asyncio_ddd.shared.domain.entities.user import User
 from asyncio_ddd.shared.domain.repositories.user_repository import UserRepository
 
@@ -5,3 +7,6 @@ from asyncio_ddd.shared.domain.repositories.user_repository import UserRepositor
 class FakeUserRepository(UserRepository):
     async def save(self, user: User) -> None:
         pass
+
+    async def retrieve(self, user_id: UUID4) -> User:
+        return User(user_id=user_id)
