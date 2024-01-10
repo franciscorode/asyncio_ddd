76e365b0de6cfd6594476a5c1a69db16a7300d9d
franciscorode <fran_rode@hotmail.com>
2024-01-10 19:37:32 +0100

    ✅ test: add missing integration tests

--- tests/shared/integration/test_user_repository.py
+++ tests/shared/integration/test_user_repository.py
@@ -4,6 +4,7 @@ from asyncio_ddd.shared.domain.entities.user import User
 from asyncio_ddd.shared.domain.repositories.user_repository import UserRepository
 from asyncio_ddd.shared.infrastructure.persistence.repositories import SqlUserRepository
 from asyncio_ddd.user.create.domain.errors import UserAlreadyExistError
+from asyncio_ddd.user.retrieve.domain.errors import UserNotFoundError
 from tests.shared.object_mothers.user_mother import UserMother
 
 
@@ -32,3 +33,17 @@ class TestUserRepository:
 
         with pytest.raises(UserAlreadyExistError):
             await self.repository.save(user=self.user)
+
+    async def should_retrieve_an_user_successfully(self, database):
+        self.repository = self._get_repository(database)
+        await self.repository.save(user=self.user)
+        result = await self.repository.retrieve(user_id=self.user.user_id)
+
+        assert isinstance(result, User)
+        assert result == self.user
+
+    async def should_raise_user_not_found_error(self, database):
+        self.repository = self._get_repository(database)
+
+        with pytest.raises(UserNotFoundError):
+            await self.repository.retrieve(user_id=self.user.user_id)
